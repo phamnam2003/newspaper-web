1fd3b1ea5ce219c667c3f812bec08bed1102a294
Pham Hai Nam <namphamhai7@gmail.com>
2022-10-28 20:23:06 +0700

    Completed Add news

--- src/app/controllers/AddController.js
+++ src/app/controllers/AddController.js
@@ -38,6 +38,32 @@ class AddController {
             })
         }
         else {
+            const politics = ['Chính trị', 'chính trị', 'chinh tri', 'Chinh tri', 'Chính Trị', 'chính Trị', 'chinh Tri', 'ct', 'CT', 'cT', 'Ct', ];
+            const military = ['Quân sự', 'quân sự', 'quân Sự', 'quan su', 'Quan Su', 'Quan su', 'qs', 'Qs', 'qS', 'QS'];
+            const education = ['Giáo dục', 'giáo dục', 'giáo Dục', 'Giao duc', 'giao Duc', 'Giao Duc', 'gd', 'Gd', 'GD', 'gD'];
+            const enviroment = ['Môi trường', 'môi trường', 'môi Trường', 'Moi truong', 'Moi Truong', 'moi Truong', 'mt', 'Mt', 'MT', 'mT'];
+            const sport = ['Thể thao', 'Thể Thao', 'thể thao', 'the thao', 'The thao', 'The Thao', 'tt', 'TT', 'Tt', 'tT'];
+            const medical = ['Y tế', 'y tế', 'y Tế', 'Y te', 'Y Te', 'y te', 'y Te', 'yt', 'Yt', 'YT', 'yT'];
+
+            if (politics.includes(category)) {
+                category = 'polictics';
+            }
+            else if (military.includes(category)) {
+                category = 'military';
+            }
+            else if (education.includes(category)) {
+                category = 'education';
+            }
+            else if (enviroment.includes(category)) {
+                category = 'enviroment';
+            }
+            else if (sport.includes(category)) {
+                category = 'sports';
+            }
+            else if (medical.includes(category)) {
+                category = 'medical';
+            }
+
             // Insert data to database
             Newspaper.create({ title,description, content, category,imgavatar,imgdesc })
                 .then(() => res.redirect('/'))
